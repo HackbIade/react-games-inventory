e313231ac2b949c6e93cbd5a5d7fe9ce8db913b7
Arnold Salazar <arnoldsalazar@MacBook-Pro-de-Arnold.local>
2022-10-05 13:47:28 -0500

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "@mui/material";
 import { ThemeProvider as StyledProvider } from "styled-components";
+import { AppRouter } from "./router/AppRouter";
 
 import { theme } from "./themes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <StyledProvider theme={theme}></StyledProvider>
+      <StyledProvider theme={theme}>
+        <AppRouter />
+      </StyledProvider>
     </ThemeProvider>
   );
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export const Home = () => {
+  return <Typography color="primary">home</Typography>;
+};
